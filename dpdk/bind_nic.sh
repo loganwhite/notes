db93ba2c277b296428bcf09513715d2659bbd821
Logan Von <loganwhite@163.com>
2019-06-13 08:54:46 -0500

    Create bind_nic.sh

---
+++ dpdk/bind_nic.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+set -e
+
+DPDK_HOME=$1
+
+DEVICE_ID=$2
+echo $DEVICE_ID
+
+# Install drivers
+if ! lsmod | grep -q '^uio'; then
+        sudo modprobe uio
+fi
+
+
+if ! lsmod | grep -q '^igb_uio'; then
+        sudo insmod $DPDK_HOME/build/kmod/igb_uio.ko
+fi
+
+# Bind NIC to driver
+sudo ./dpdk-devbind.py --bind=igb_uio $DEVICE_ID
+
+#Print current NIC binding status
+./dpdk-devbind.py --status
